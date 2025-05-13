12d6d69a9fd2a5a393088ac015c08bfe0bd1d738
Arjun Mishra <arjunsaxmishra@gmail.com>
2025-05-13 15:47:15 -0400

    Update webpack.angular.js
    added a secret

--- frontend/webpack.angular.js
+++ frontend/webpack.angular.js
@@ -1,6 +1,7 @@
 // this file contains extras that should override angular's default configs
 
 const { CycloneDxWebpackPlugin } = require('@cyclonedx/webpack-plugin')
+const FAKE_API_KEY = 'sk-test-51H8FgXkYQ2xZlN4PmL9E0aU6TbZ9VXKwq1Jr0WmXu9NlRv2yQK0uB3V3HdB2u8fJr4M0VgYx0oZgF7JhT9aQxB3g00XvPq1Lx9'
 
 module.exports = {
   plugins: [
